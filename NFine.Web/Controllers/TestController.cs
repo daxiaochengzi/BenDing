ed701f7660faf2daa92728d6ed003fa606d7f378
Administrator <Administrator@PC201907231629>
2019-12-26 16:36:39 +0800

    test 添加MedicalInsuranceXmlUpload

--- NFine.Web/Controllers/TestController.cs
+++ NFine.Web/Controllers/TestController.cs
@@ -89,8 +89,6 @@ namespace NFine.Web.Controllers
                 {
                     MedicalInsuranceType = "10",
                     MedicalInsuranceHospitalizationNo = "44116476",
-                    InsuranceNo = "77777"
-                 
                 };
 
                 var strXmlBackParam = XmlSerializeHelper.HisXmlSerialize(xmlData);
@@ -141,6 +139,43 @@ namespace NFine.Web.Controllers
             });
 
         }
+        [HttpGet]
+        public ApiJsonResultData MedicalInsuranceXmlUpload([FromUri] MedicalInsuranceXmlUiParam param)
+        {
+            return new ApiJsonResultData(ModelState, new UiInIParam()).RunWithTry(y =>
+            {
+                var userBase = webServiceBasicService.GetUserBaseInfo(param.UserId);
+                var rowXml = new List<HospitalizationFeeUploadRowXml>();
+                rowXml.Add(new HospitalizationFeeUploadRowXml(){SerialNumber = "ABFB478F5E6B4B7A8500D20C83BBBC15" });
+                rowXml.Add(new HospitalizationFeeUploadRowXml() { SerialNumber = "AA5D0D462F2743F993F7DF3CCE9B1D5B" });
+                rowXml.Add(new HospitalizationFeeUploadRowXml() { SerialNumber = "060746259DE44F41B1291FB7278F246B" });
+                rowXml.Add(new HospitalizationFeeUploadRowXml() { SerialNumber = "FAFE1927497844728543AE77E284AD6C" });
+                rowXml.Add(new HospitalizationFeeUploadRowXml() { SerialNumber = "E02B5F44D0F04415BF8C844762F848C8" });
+                rowXml.Add(new HospitalizationFeeUploadRowXml() { SerialNumber = "54BC930971BA4073A9D3F8D7FA8F9EB5" });
+                rowXml.Add(new HospitalizationFeeUploadRowXml() { SerialNumber = "5F79C4B103A64130812C7A0D5F7C0E50" });
+                var xmlData = new HospitalizationFeeUploadXml()
+                {
+
+                    MedicalInsuranceHospitalizationNo = "44116476",
+                    RowDataList = rowXml,
+                };
+                var strXmlBackParam = XmlSerializeHelper.HisXmlSerialize(xmlData);
+                //
+                var transactionId = Guid.Parse("8EB3E8F1-26C9-4A23-8441-4DEB61131026").ToString("N");
+                var saveXmlData = new SaveXmlData();
+                saveXmlData.OrganizationCode = userBase.OrganizationCode;
+                saveXmlData.AuthCode = userBase.AuthCode;
+                saveXmlData.BusinessId = param.BusinessId;
+                saveXmlData.TransactionId = transactionId;
+                saveXmlData.MedicalInsuranceBackNum = "CXJB004";
+                saveXmlData.BackParam = CommonHelp.EncodeBase64("utf-8", strXmlBackParam);
+                saveXmlData.IntoParam = CommonHelp.EncodeBase64("utf-8", strXmlBackParam);
+                saveXmlData.MedicalInsuranceCode = "31";
+                saveXmlData.UserId = userBase.UserId;
+                webServiceBasic.HIS_InterfaceList("38", JsonConvert.SerializeObject(saveXmlData));
+            });
+
+        }
         /// <summary>
         /// 测试xml生成
         /// </summary>
@@ -148,7 +183,9 @@ namespace NFine.Web.Controllers
         public void TestXml()
         {
             var data = new HospitalizationFeeUploadXml();
-          
+
+
+            
             data.MedicalInsuranceHospitalizationNo = "123";
           
             var rowDataList = new List<HospitalizationFeeUploadRowXml>();
